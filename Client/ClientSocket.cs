7c33308bb533de39619b6810ad9378bae9a04c6a
Igor Dalkiewicz <igortenn@gmail.com>
2022-07-05 19:43:08 +0200

    Add project files.

---
+++ Client/ClientSocket.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class ClientSocket
+    {
+
+        static void Main(string[] args)
+        {
+            ExecuteClient();
+        }
+
+        static void ExecuteClient()
+        {
+            try
+            {
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = ipHost.AddressList[0];
+                IPEndPoint localEndpoint = new IPEndPoint(ipAddress, 11111);
+
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    sender.Connect(localEndpoint);
+                    Console.WriteLine("Socket connected to -> {0}", sender.RemoteEndPoint.ToString());
+
+                    byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
+                    int byteSent = sender.Send(messageSent);
+
+                    byte[] messageReceived = new byte[1024];
+
+                    int byteRcvd = sender.Receive(messageReceived);
+                    Console.WriteLine("Message from server -> {0}", Encoding.ASCII.GetString(messageReceived, 0, byteRcvd));
+
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+                catch (ArgumentNullException ane)
+                {
+
+                }
+                
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+        }
+    }
+}
