e4112fd38ff291d3c5bc3245cbb5e2c4e159fd34
Igor Dalkiewicz <igortenn@gmail.com>
2022-07-07 12:16:11 +0200

    Added commands and JSON. TO-DO: split commands text to multiple lines, implement closing socket for client after "stop" command

--- Client/ClientSocket.cs
+++ Client/ClientSocket.cs
@@ -2,12 +2,13 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace Client
 {
     class ClientSocket
     {
-
+        
         static void Main(string[] args)
         {
             ExecuteClient();
@@ -21,6 +22,8 @@ namespace Client
                 IPAddress ipAddress = ipHost.AddressList[0];
                 IPEndPoint localEndpoint = new IPEndPoint(ipAddress, 11111);
 
+                bool flag = true;
+
                 Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
                 try
@@ -28,17 +31,37 @@ namespace Client
                     sender.Connect(localEndpoint);
                     Console.WriteLine("Socket connected to -> {0}", sender.RemoteEndPoint.ToString());
 
-                    byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
-                    int byteSent = sender.Send(messageSent);
+                    while (true)
+                    {
 
-                    byte[] messageReceived = new byte[1024];
 
-                    int byteRcvd = sender.Receive(messageReceived);
-                    Console.WriteLine("Message from server -> {0}", Encoding.ASCII.GetString(messageReceived, 0, byteRcvd));
+                        Console.Write("Enter a command: ");
+                        string command = null;
+                        command = Console.ReadLine();
 
+                        byte[] messageSent = Encoding.ASCII.GetBytes(command);
+                        int byteSent = sender.Send(messageSent);
+
+                        byte[] messageReceived = new byte[1024];
+
+                        int byteRcvd = sender.Receive(messageReceived);
+                        //Console.WriteLine("Message from server -> {0}", Encoding.ASCII.GetString(messageReceived, 0, byteRcvd));
+
+                        string encodingString = Encoding.ASCII.GetString(messageReceived, 0, byteRcvd);
+                        string jasonCommand = JsonConvert.SerializeObject(encodingString);
+                        
+                        Console.WriteLine(jasonCommand);
+
+                        if (jasonCommand == "stop") {
+
+                            break;
+                        }
+
+                    } 
                     sender.Shutdown(SocketShutdown.Both);
                     sender.Close();
                 }
+
                 catch (ArgumentNullException ane)
                 {
                     Console.WriteLine("ArgumentNullException ; {0}", ane.ToString());
@@ -54,7 +77,8 @@ namespace Client
                     Console.WriteLine("Unexpected exception : {0}", e.ToString());
                 }
 
-                
+
+
             }
             catch (Exception e)
             {
--- Server/ServerSocket.cs
+++ Server/ServerSocket.cs
@@ -14,6 +14,9 @@ namespace Server
 
         public static void ExecuteServer()
         {
+            string serverVersion = "0.0.1";
+            DateTime serverCreationDate = DateTime.Now;
+
             IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
             IPAddress ipAddress = ipHost.AddressList[0];
             IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11111);
@@ -30,23 +33,50 @@ namespace Server
 
                     Socket clientSocket = listener.Accept();
 
-                    byte[] bytes = new byte[1024];
-                    string data = null;
+                    Console.WriteLine("Connected");
                     while (true)
                     {
+                        byte[] bytes = new byte[1024];
+                        string data = null;
+
                         int numByte = clientSocket.Receive(bytes);
                         data += Encoding.ASCII.GetString(bytes, 0, numByte);
+                        Console.WriteLine("Text received -> {0}", data);
+                        byte[] message;
+                        switch(data.ToLower())
+                        {
+                            case "help":
+                                message = Encoding.ASCII.GetBytes($"Available commands:" +
+                                    $"'info' - to get info about server version, server creation date" +
+                                    $"'help' - to get a list of available commands with their description" +
+                                    $"'uptime' - to check server uptime" +
+                                    $"'stop' - to stop the server");
+                                clientSocket.Send(message);
+                                break;
+                            case "info":
+                                message = Encoding.ASCII.GetBytes($"Server version: {serverVersion}" +
+                                    $"Server Creation Date: {serverCreationDate}");
+                                clientSocket.Send(message);
+                                break;
+                            case "uptime":
+                                DateTime serverCurrentDate = DateTime.Now;
+                                message = Encoding.ASCII.GetBytes($"Server is up for {serverCurrentDate - serverCreationDate}");
+                                clientSocket.Send(message);
+                                break;
+                            case "stop":
+                                message = Encoding.ASCII.GetBytes("stop");
+                                clientSocket.Send(message);
+                                clientSocket.Shutdown(SocketShutdown.Both);
+                                clientSocket.Close();
 
-                        if (data.IndexOf("<EOF>") > -1)
-                            break;
+                                break;
+                            default:
+                                message = Encoding.ASCII.GetBytes($"Incorrect command. Type 'help' to get list of commands.");
+                                clientSocket.Send(message);
+                                break;
+                        }
                     }
-                    Console.WriteLine("Text received -> {0}", data);
-                    byte[] message = Encoding.ASCII.GetBytes("Test Server");
 
-                    clientSocket.Send(message);
-
-                    clientSocket.Shutdown(SocketShutdown.Both);
-                    clientSocket.Close();
                 }
             }
             catch (Exception ex)
