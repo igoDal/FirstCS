132e8e1ef81b7a2d8c09085f3f7474e3859dfaff
GosiaLek <gosngos@gmail.com>
2022-07-16 23:48:45 +0200

    Working on "log in".

--- Client/ClientSocket.cs
+++ Client/ClientSocket.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -23,6 +24,8 @@ namespace Client
 
                 Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
+                bool isLoggedIn = false;
+
                 try
                 {
                     sender.Connect(localEndpoint);
@@ -30,30 +33,39 @@ namespace Client
 
                     while (true)
                     {
-                        Console.Write("Enter a command: ");
-                        string command = Console.ReadLine();
+                        Console.WriteLine("Enter username: ");
+                        string username = Console.ReadLine();
+                        Console.WriteLine("Enter password: ");
+                        string password = Console.ReadLine();
 
-                        byte[] messageSent = Encoding.ASCII.GetBytes(command);
-                        int byteSent = sender.Send(messageSent);
+                        if (File.Exists($"C:/Users/Igor/source/repos/igoDal/FirstCS{username}.txt") 
+                            && password.Equals(File.ReadAllText($"C:/Users/Igor/source/repos/igoDal/FirstCS{ username}.txt")))
+                        {
+                            Console.Write("Enter a command: ");
+                            string command = Console.ReadLine();
 
-                        byte[] messageReceived = new byte[1024];
+                            byte[] messageSent = Encoding.ASCII.GetBytes(command);
+                            int byteSent = sender.Send(messageSent);
 
-                        int byteRcvd = sender.Receive(messageReceived);
+                            byte[] messageReceived = new byte[1024];
 
-                        //Still need to figure out why it takes quotes from server command.
-                        string encodingString = Encoding.ASCII.GetString(messageReceived, 0, byteRcvd);
-                        string jasonCommand = JsonConvert.SerializeObject(encodingString);
-                        
-                        Console.WriteLine(jasonCommand);
-                        
-                        if (jasonCommand == "\"stop\"") {
-                            sender.Shutdown(SocketShutdown.Both);
-                            sender.Close();
-                            break;
+                            int byteRcvd = sender.Receive(messageReceived);
+
+                            //Still need to figure out why it takes quotes from server command.
+                            string encodingString = Encoding.ASCII.GetString(messageReceived, 0, byteRcvd);
+                            string jasonCommand = JsonConvert.SerializeObject(encodingString);
+
+                            Console.WriteLine(jasonCommand);
+
+                            if (jasonCommand == "\"stop\"")
+                            {
+                                sender.Shutdown(SocketShutdown.Both);
+                                sender.Close();
+                                break;
+                            }
                         }
-                    } 
+                    }
                 }
-
                 catch (ArgumentNullException ane)
                 {
                     Console.WriteLine("ArgumentNullException ; {0}", ane.ToString());
