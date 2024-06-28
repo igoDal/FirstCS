c1c5b9d1cce418a738dd651e79f378355ba28c6d
igodal <igortenn@gmail.com>
2024-06-28 23:41:18 +0200

    Separated UserService from Server Socket

---
+++ Server/UserService.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace Server;
+public class UserService
+{
+    public string AddUser(string username, string password)
+    {
+        if (File.Exists($"{username}.json"))
+        {
+            return $"User {username} already exists.";
+        }
+
+        User user = new User()
+        {
+            Userame = username,
+            Password = password,
+            Role = "user"
+        };
+
+        using (StreamWriter file = File.CreateText($"{username}.json"))
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Serialize(file, user);
+        }
+
+        return $"User {username} has been added.";
+    }
+}
