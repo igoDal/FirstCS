44c2f7616376fd4524d30f118aac9c70f9beb99e
igodal <igortenn@gmail.com>
2024-07-18 23:18:48 +0200

    Refactored Client's SendMessage and ReadMessage

---
+++ Client/Interfaces/IMessageService.cs
@@ -0,0 +1,7 @@
+﻿namespace Client.Interfaces;
+
+public interface IMessageService
+{
+    void SendMessage(string command, ISocketWrapper socketWrapper);
+    void ReadMessage(string command, ISocketWrapper socketWrapper);
+}
