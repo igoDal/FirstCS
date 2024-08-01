7725af95bc31f9f8c23b98abe51b52d78fde2ec9
igodal <igortenn@gmail.com>
2024-08-01 23:14:37 +0200

    Finished Client's UserService tests

--- Client.Test/UserServiceTests.cs
+++ Client.Test/UserServiceTests.cs
@@ -7,185 +7,208 @@ using Newtonsoft.Json;
 namespace Client.Test;
 
 public class UserServiceTests
+{
+    private readonly Mock<ISocketWrapper> _mockSocketWrapper;
+    private readonly UserService _userService;
+
+    public UserServiceTests()
     {
-        private readonly Mock<ISocketWrapper> _mockSocketWrapper;
-        private readonly UserService _userService;
+        _mockSocketWrapper = new Mock<ISocketWrapper>();
+        _userService = new UserService(_mockSocketWrapper.Object);
+    }
+    
+    [Fact]
+    public void DeleteUser_ShouldSendSuccessMessage_WhenUserIsDeletedSuccessfully()
+    {
+        // Arrange
+        var username = "userToDelete";
+        var result = $"User {username} has been deleted.";
+        File.WriteAllText($"{username}.json", "{ \"Userame\": \"userToDelete\", \"Password\": \"password\", \"Role\": \"user\" }");
 
-        public UserServiceTests()
+        var inputSequence = new Queue<string>(new[] { username });
+        Console.SetIn(new StringReader(string.Join(Environment.NewLine, inputSequence)));
+
+        // Act
+        var deleteResult = _userService.DeleteUser(username);
+
+        // Assert
+        Assert.Equal(result, deleteResult);
+
+        if (File.Exists($"{username}.json"))
         {
-            _mockSocketWrapper = new Mock<ISocketWrapper>();
-            _userService = new UserService(_mockSocketWrapper.Object);
-        }
-
-        [Fact]
-        public void Login_ShouldReturnTrue_WhenCredentialsAreCorrect()
-        {
-            // Arrange
-            var username = "testuser";
-            var password = "password123";
-            var loginPrompt = JsonConvert.SerializeObject(new { command = "Enter username:" });
-            var passwordPrompt = JsonConvert.SerializeObject(new { command = "Enter password:" });
-            var loginResponse = JsonConvert.SerializeObject(new { command = "loggedIn" });
-
-            var responseQueue = new Queue<string>();
-            responseQueue.Enqueue(loginPrompt);
-            responseQueue.Enqueue(passwordPrompt);
-            responseQueue.Enqueue(loginResponse);
-
-            _mockSocketWrapper.Setup(m => m.Receive(It.IsAny<byte[]>()))
-                .Returns((byte[] buffer) =>
-                {
-                    var response = responseQueue.Dequeue();
-                    var bytes = Encoding.ASCII.GetBytes(response);
-                    Array.Copy(bytes, buffer, bytes.Length);
-                    return bytes.Length;
-                });
-
-            // Act
-            var result = _userService.Login();
-
-            // Assert
-            Assert.True(result);
-            _mockSocketWrapper.Verify(m => m.Send(It.Is<byte[]>(b => Encoding.ASCII.GetString(b).Contains(username))), Times.Once);
-            _mockSocketWrapper.Verify(m => m.Send(It.Is<byte[]>(b => Encoding.ASCII.GetString(b).Contains(password))), Times.Once);
-        }
-
-        [Fact]
-        public void Login_ShouldReturnFalse_WhenCredentialsAreIncorrect()
-        {
-            // Arrange
-            var username = "testuser";
-            var password = "wrongpassword";
-            var loginPrompt = JsonConvert.SerializeObject(new { command = "Enter username:" });
-            var passwordPrompt = JsonConvert.SerializeObject(new { command = "Enter password:" });
-            var loginResponse = JsonConvert.SerializeObject(new { command = "Incorrect password!" });
-
-            var responseQueue = new Queue<string>();
-            responseQueue.Enqueue(loginPrompt);
-            responseQueue.Enqueue(passwordPrompt);
-            responseQueue.Enqueue(loginResponse);
-
-            _mockSocketWrapper.Setup(m => m.Receive(It.IsAny<byte[]>()))
-                .Returns((byte[] buffer) =>
-                {
-                    var response = responseQueue.Dequeue();
-                    var bytes = Encoding.ASCII.GetBytes(response);
-                    Array.Copy(bytes, buffer, bytes.Length);
-                    return bytes.Length;
-                });
-
-            // Act
-            var result = _userService.Login();
-
-            // Assert
-            Assert.False(result);
-            _mockSocketWrapper.Verify(m => m.Send(It.Is<byte[]>(b => Encoding.ASCII.GetString(b).Contains(username))), Times.Once);
-            _mockSocketWrapper.Verify(m => m.Send(It.Is<byte[]>(b => Encoding.ASCII.GetString(b).Contains(password))), Times.Once);
-        }
-
-        [Fact]
-        public void Login_ShouldReturnFalse_WhenUserDoesNotExist()
-        {
-            // Arrange
-            var username = "nonexistentuser";
-            var loginPrompt = JsonConvert.SerializeObject(new { command = "Enter username:" });
-            var passwordPrompt = JsonConvert.SerializeObject(new { command = "Enter password:" });
-            var loginResponse = JsonConvert.SerializeObject(new { command = "User doesn't exist." });
-
-            var responseQueue = new Queue<string>();
-            responseQueue.Enqueue(loginPrompt);
-            responseQueue.Enqueue(passwordPrompt);
-            responseQueue.Enqueue(loginResponse);
-
-            _mockSocketWrapper.Setup(m => m.Receive(It.IsAny<byte[]>()))
-                .Returns((byte[] buffer) =>
-                {
-                    var response = responseQueue.Dequeue();
-                    var bytes = Encoding.ASCII.GetBytes(response);
-                    Array.Copy(bytes, buffer, bytes.Length);
-                    return bytes.Length;
-                });
-
-            // Act
-            var result = _userService.Login();
-
-            // Assert
-            Assert.False(result);
-            _mockSocketWrapper.Verify(m => m.Send(It.Is<byte[]>(b => Encoding.ASCII.GetString(b).Contains(username))), Times.Once);
-        }
-        
-        [Fact]
-        public void DeleteUser_ShouldSendSuccessMessage_WhenUserIsDeletedSuccessfully()
-        {
-            // Arrange
-            var username = "userToDelete";
-            var result = $"User {username} has been deleted.";
-            File.WriteAllText($"{username}.json", "{ \"Userame\": \"userToDelete\", \"Password\": \"password\", \"Role\": \"user\" }");
-
-            var inputSequence = new Queue<string>(new[] { username });
-            Console.SetIn(new StringReader(string.Join(Environment.NewLine, inputSequence)));
-
-            // Act
-            var deleteResult = _userService.DeleteUser(username);
-
-            // Assert
-            Assert.Equal(result, deleteResult);
-
-            if (File.Exists($"{username}.json"))
-            {
-                File.Delete($"{username}.json");
-            }
-        }
-        [Fact]
-        public void DeleteUser_ShouldSendFailureMessage_WhenUserDoesNotExist()
-        {
-            // Arrange
-            var username = "nonexistentuser";
-            var result = $"User {username} does not exist.";
-
-            var inputSequence = new Queue<string>(new[] { username });
-            Console.SetIn(new StringReader(string.Join(Environment.NewLine, inputSequence)));
-
-            // Act
-            var deleteResult = _userService.DeleteUser(username);
-
-            // Assert
-            Assert.Equal(result, deleteResult);
-        }
-        
-        [Fact]
-        public void GetUserInfo_ShouldReturnCorrectUserInfo_WhenUserExists()
-        {
-            // Arrange
-            var username = "testuser";
-            var password = "password123";
-            var role = "user";
-            var expectedUserInfo = $"Username: {username}\nPassword: {password}\nRole: {role}";
-            File.WriteAllText($"{username}.json", $"{{ \"Userame\": \"{username}\", \"Password\": \"{password}\", \"Role\": \"{role}\" }}");
-
-            // Act
-            var userInfo = _userService.GetUserInfo(username);
-
-            // Assert
-            Assert.Equal(expectedUserInfo, userInfo);
-
-            if (File.Exists($"{username}.json"))
-            {
-                File.Delete($"{username}.json");
-            }
-        }
-
-        [Fact]
-        public void GetUserInfo_ShouldReturnErrorMessage_WhenUserDoesNotExist()
-        {
-            // Arrange
-            var username = "nonexistentuser";
-            var expectedMessage = "User file not found.";
-
-            // Act
-            var userInfo = _userService.GetUserInfo(username);
-
-            // Assert
-            Assert.Equal(expectedMessage, userInfo);
+            File.Delete($"{username}.json");
         }
     }
+    [Fact]
+    public void DeleteUser_ShouldSendFailureMessage_WhenUserDoesNotExist()
+    {
+        // Arrange
+        var username = "nonexistentuser";
+        var result = $"User {username} does not exist.";
+
+        var inputSequence = new Queue<string>(new[] { username });
+        Console.SetIn(new StringReader(string.Join(Environment.NewLine, inputSequence)));
+
+        // Act
+        var deleteResult = _userService.DeleteUser(username);
+
+        // Assert
+        Assert.Equal(result, deleteResult);
+    }
+    
+    [Fact]
+    public void AddUser_ShouldReturnSuccessMessage_WhenUserIsAddedSuccessfully()
+    {
+        // Arrange
+        var username = "newuser";
+        var password = "password123";
+        var expectedMessage = $"User {username} has been added.";
+
+        if (File.Exists($"{username}.json"))
+        {
+            File.Delete($"{username}.json");
+        }
+
+        // Act
+        var result = _userService.AddUser(username, password);
+
+        // Assert
+        Assert.Equal(expectedMessage, result);
+        Assert.True(File.Exists($"{username}.json"));
+
+        // Clean up
+        if (File.Exists($"{username}.json"))
+        {
+            File.Delete($"{username}.json");
+        }
+    }
+
+    [Fact]
+    public void AddUser_ShouldReturnErrorMessage_WhenUserAlreadyExists()
+    {
+        // Arrange
+        var username = "existinguser";
+        var password = "password123";
+        var expectedMessage = $"User {username} already exists.";
+
+        File.WriteAllText($"{username}.json", $"{{ \"Userame\": \"{username}\", \"Password\": \"{password}\", \"Role\": \"user\" }}");
+
+        // Act
+        var result = _userService.AddUser(username, password);
+
+        // Assert
+        Assert.Equal(expectedMessage, result);
+
+        // Clean up
+        if (File.Exists($"{username}.json"))
+        {
+            File.Delete($"{username}.json");
+        }
+    }
+    
+    [Fact]
+    public void GetUserInfo_ShouldReturnCorrectUserInfo_WhenUserExists()
+    {
+        // Arrange
+        var username = "testuser";
+        var password = "password123";
+        var role = "user";
+        var expectedUserInfo = $"Username: {username}\nPassword: {password}\nRole: {role}";
+        File.WriteAllText($"{username}.json", $"{{ \"Userame\": \"{username}\", \"Password\": \"{password}\", \"Role\": \"{role}\" }}");
+
+        // Act
+        var userInfo = _userService.GetUserInfo(username);
+
+        // Assert
+        Assert.Equal(expectedUserInfo, userInfo);
+
+        if (File.Exists($"{username}.json"))
+        {
+            File.Delete($"{username}.json");
+        }
+    }
+
+    [Fact]
+    public void GetUserInfo_ShouldReturnErrorMessage_WhenUserDoesNotExist()
+    {
+        // Arrange
+        var username = "nonexistentuser";
+        var expectedMessage = "User file not found.";
+
+        // Act
+        var userInfo = _userService.GetUserInfo(username);
+
+        // Assert
+        Assert.Equal(expectedMessage, userInfo);
+    }
+    
+    [Fact]
+    public void Login_ShouldReturnTrue_WhenCredentialsAreCorrect()
+    {
+        // Arrange
+        var username = "testuser";
+        var password = "password123";
+        var loginPrompt = JsonConvert.SerializeObject(new { command = "Enter username:" });
+        var passwordPrompt = JsonConvert.SerializeObject(new { command = "Enter password:" });
+        var loginResponse = JsonConvert.SerializeObject(new { command = "loggedIn" });
+
+        _mockSocketWrapper.Setup(m => m.Receive(It.IsAny<byte[]>()))
+            .Callback<byte[]>(buffer => Encoding.ASCII.GetBytes(loginPrompt).CopyTo(buffer, 0))
+            .Returns(loginPrompt.Length);
+
+        _mockSocketWrapper.Setup(m => m.Receive(It.IsAny<byte[]>()))
+            .Callback<byte[]>(buffer => Encoding.ASCII.GetBytes(passwordPrompt).CopyTo(buffer, 0))
+            .Returns(passwordPrompt.Length);
+
+        _mockSocketWrapper.Setup(m => m.Receive(It.IsAny<byte[]>()))
+            .Callback<byte[]>(buffer => Encoding.ASCII.GetBytes(loginResponse).CopyTo(buffer, 0))
+            .Returns(loginResponse.Length);
+        
+        var inputSequence = new Queue<string>(new[] { username, password });
+        Console.SetIn(new StringReader(string.Join(Environment.NewLine, inputSequence)));
+
+        // Act
+        var result = _userService.Login();
+
+        // Assert
+        Assert.True(result);
+        Assert.Equal(username, _userService.GetLoggedInUser());
+
+        _mockSocketWrapper.Verify(m => m.Send(It.IsAny<byte[]>()), Times.Exactly(2));
+    }
+    
+    [Fact]
+    public void Login_ShouldReturnFalse_WhenUserDoesNotExist()
+    {
+        // Arrange
+        var username = "nonexistentuser";
+        var password = "password123";
+        var loginPrompt = JsonConvert.SerializeObject(new { command = "Enter username:" });
+        var passwordPrompt = JsonConvert.SerializeObject(new { command = "Enter password:" });
+        var loginResponse = JsonConvert.SerializeObject(new { command = "User doesn't exist." });
+
+        _mockSocketWrapper.Setup(m => m.Receive(It.IsAny<byte[]>()))
+            .Callback<byte[]>(buffer => Encoding.ASCII.GetBytes(loginPrompt).CopyTo(buffer, 0))
+            .Returns(loginPrompt.Length);
+
+        _mockSocketWrapper.Setup(m => m.Receive(It.IsAny<byte[]>()))
+            .Callback<byte[]>(buffer => Encoding.ASCII.GetBytes(passwordPrompt).CopyTo(buffer, 0))
+            .Returns(passwordPrompt.Length);
+
+        _mockSocketWrapper.Setup(m => m.Receive(It.IsAny<byte[]>()))
+            .Callback<byte[]>(buffer => Encoding.ASCII.GetBytes(loginResponse).CopyTo(buffer, 0))
+            .Returns(loginResponse.Length);
+
+        var inputSequence = new Queue<string>(new[] { username, password });
+        Console.SetIn(new StringReader(string.Join(Environment.NewLine, inputSequence)));
+
+        // Act
+        var result = _userService.Login();
+
+        // Assert
+        Assert.False(result);
+        Assert.Null(_userService.GetLoggedInUser());
+
+        _mockSocketWrapper.Verify(m => m.Send(It.IsAny<byte[]>()), Times.Exactly(2));
+    }
+}
